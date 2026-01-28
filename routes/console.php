90a7f444047fbbc9e9cbae617291d33fc7ded3fa
Akhmad <akhmadshaleh@gmail.com>
2026-01-28 16:29:37 +0700

    add queue schedular for demo

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('queue:work --stop-when-empty')
+    ->environments(['production'])
+    ->everyMinute()
+    ->withoutOverlapping();
