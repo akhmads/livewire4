5c48b12227e0ce1cbbbbde7ab89770d80ce170d2
Akhmad <akhmadshaleh@gmail.com>
2026-02-03 17:10:24 +0700

    update order

--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -20,7 +20,7 @@ class ContactSeeder extends Seeder
                 'name' => $faker->name(),
                 'email' => $faker->unique()->safeEmail(),
                 'phone' => $faker->phoneNumber(),
-                'mobile' => $faker->optional(0.8)->mobileNumber(), // 80% chance to have mobile
+                'mobile' => $faker->optional(0.8)->phoneNumber(), // 80% chance to have mobile
                 'address' => $faker->optional(0.7)->address(), // 70% chance to have address
             ]);
         }
